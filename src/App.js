fc9cc30ebb77a0f08d029874ef2e57a4aa45f830
princehabeeb <habiy3400@gmail.com>
2023-11-04 22:36:25 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Hero from './pages/Hero';
+import '../src/styles/App.css';
+import IMG1 from '../src/Images/img2.jpg';
+import { CiFacebook } from 'react-icons/ci';
+import { AiFillTwitterCircle } from 'react-icons/ai';
+import { BiLogoGooglePlusCircle } from 'react-icons/bi';
+import { FaLinkedinIn } from 'react-icons/fa';
 
 const App = () => {
   return (
-    <>
-     <Navbar />
-     <BrowserRouter>
-     <Routes>
-      <Route path='/'  />
-      <Route path='/about' element={<About />}/>
-      <Route path='/contact' element={<Contact />}/>
-      <Route path='/login' element={<Login />}  />
-     </Routes>
-     </BrowserRouter>
-     <Hero name='Our try'  style={{color:'red'}} >
-      udcdycyc
-     </Hero>
-    </>
+    <section className='container'>
+      {/* Card One */}
+      <div className='card'>
+      <img src={IMG1} />
+      <h1>Williamson</h1>
+      <p>Web Developer</p>
+      <div className='social-icons'>
+        <div>
+        <CiFacebook />
+        </div>
+        <div>
+          <AiFillTwitterCircle />
+        </div>
+        <div>
+          <BiLogoGooglePlusCircle />
+        </div>
+        <div>
+          <FaLinkedinIn />
+        </div>
+      </div>
+    </div>
+{/* Card One */}
+{/* Card One */}
+<div className='card'>
+      <img src={IMG1} />
+      <h1>Williamson</h1>
+      <p>Web Developer</p>
+      <div className='social-icons'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+{/* Card One */}
+{/* Card One */}
+<div className='card'>
+      <img src={IMG1} />
+      <h1>Williamson</h1>
+      <p>Web Developer</p>
+      <div className='social-icons'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+{/* Card One */}
+{/* Card One */}
+<div className='card'>
+      <img src={IMG1} />
+      <h1>Williamson</h1>
+      <p>Web Developer</p>
+      <div className='social-icons'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+{/* Card One */}
+    </section>
+    
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BrowserRouter, NavLink, Routes} from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
     <nav>
         <h1>MOTEK</h1>
-     <div className='nav-links'>
-     <BrowserRouter >
-        <NavLink to='/' >Home</NavLink>
-        <NavLink to='/about'>About</NavLink>
-        <NavLink to='/contact'>Contact</NavLink>
-        <NavLink to='/login' >Login</NavLink>
-     </BrowserRouter>
-     </div>
+    
+<ul>
+
+  <li>
+  <Link to='/' >Home</Link>
+  </li>
+  <li>
+  <Link to='/about' >About</Link>
+  </li>
+  <li>
+  <Link to='/contact' >Contact</Link>
+  </li>
+</ul>
+       
 
     </nav>
   )
