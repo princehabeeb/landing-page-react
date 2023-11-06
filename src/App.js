3a60250d14c5e29a4c79fdae6413b452e915f190
princehabeeb <habiy3400@gmail.com>
2023-11-06 16:36:00 +0000

    mapping practice

--- src/App.js
+++ src/App.js
@@ -1,74 +1,29 @@
 import React from 'react';
 import '../src/styles/App.css';
-import IMG1 from '../src/Images/img2.jpg';
-import { CiFacebook } from 'react-icons/ci';
-import { AiFillTwitterCircle } from 'react-icons/ai';
-import { BiLogoGooglePlusCircle } from 'react-icons/bi';
-import { FaLinkedinIn } from 'react-icons/fa';
+// import IMG1 from '../src/Images/img2.jpg';
+// import { CiFacebook } from 'react-icons/ci';
+// import { AiFillTwitterCircle } from 'react-icons/ai';
+// import { BiLogoGooglePlusCircle } from 'react-icons/bi';
+// import { FaLinkedinIn } from 'react-icons/fa';
+import info from '../src/components/info';
 
 const App = () => {
   return (
     <section className='container'>
-      {/* Card One */}
-      <div className='card'>
-      <img src={IMG1} />
-      <h1>Williamson</h1>
-      <p>Web Developer</p>
-      <div className='social-icons'>
-        <div>
-        <CiFacebook />
-        </div>
-        <div>
-          <AiFillTwitterCircle />
-        </div>
-        <div>
-          <BiLogoGooglePlusCircle />
-        </div>
-        <div>
-          <FaLinkedinIn />
-        </div>
-      </div>
-    </div>
-{/* Card One */}
-{/* Card One */}
-<div className='card'>
-      <img src={IMG1} />
-      <h1>Williamson</h1>
-      <p>Web Developer</p>
-      <div className='social-icons'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-{/* Card One */}
-{/* Card One */}
-<div className='card'>
-      <img src={IMG1} />
-      <h1>Williamson</h1>
-      <p>Web Developer</p>
-      <div className='social-icons'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-{/* Card One */}
-{/* Card One */}
-<div className='card'>
-      <img src={IMG1} />
-      <h1>Williamson</h1>
-      <p>Web Developer</p>
-      <div className='social-icons'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-{/* Card One */}
+      
+      
+      {info.map((chunk) => {
+        return (
+        <div className='card' id={chunk.id} >
+        <img src={chunk.img} alt={chunk.img} />
+        <h1>{chunk.haeding}</h1>
+        <p>{chunk.paragraph}</p>
+         </div>
+        )
+        
+      })}
+    
+
     </section>
     
   )
---
+++ src/components/info.js
@@ -0,0 +1,33 @@
+import Image1 from '../Images/img2.jpg';
+import Image2 from '../Images/img3.jpg';
+import Image3 from '../Images/img4.jpg';
+import Image4 from '../Images/img5.jpg';
+
+const info = [
+    {
+        img: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGVvcGxlJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D',
+        id: 1,
+        haeding: 'Williamson',
+        paragraph: 'Web developer'
+    },
+    {
+        img: Image2,
+        id: 2,
+        haeding: 'Prince',
+        paragraph: 'Graphics designer'
+    },
+    {
+        img: Image3,
+        id: 3,
+        haeding: 'Williamson',
+        paragraph: 'Web developer'
+    },
+    {
+        img: Image4,
+        id: 4,
+        haeding: 'Williamson',
+        paragraph: 'Web developer'
+    },
+];
+
+export default info
